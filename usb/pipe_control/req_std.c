8b48897658cc1d168ee80d64a893937e35e09e82
daren <heidaren0000@hotmail.com>
2023-06-23 21:59:29 +0800

    working on std requests

--- usb/pipe_control/req_std.c
+++ usb/pipe_control/req_std.c
@@ -58,3 +58,24 @@ void req_std() {
 		break;
 	}
 }
+
+void get_status() {
+
+}
+
+void clear_feature() {}
+void set_feature() {}
+
+void set_address() {}
+void get_descriptor() {}
+void set_descriptor() {}
+
+void get_configuration() {}
+void set_configuration() {}
+
+void get_interface() {}
+void set_interface() {}
+
+void synch_frame() {
+	control_stall();
+}
